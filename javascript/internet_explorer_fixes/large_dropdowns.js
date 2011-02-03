051923d0204a99caf2156fa96e4f549923b7e92a
Tijs Verkoyen <tijs@verkoyen.eu>
2011-02-03 15:09:47 +0100

    Beter version, which didn't have wierd behaviour in IE7

--- javascript/internet_explorer_fixes/large_dropdowns.js
+++ javascript/internet_explorer_fixes/large_dropdowns.js
@@ -1,8 +1,52 @@
 /**
  * It seems IE can't display selectboxes that have a width set properly
  * 
- * When the select is entered the width wil be reset to auto, the original width will be stored, so if the mouse 
- * leaves the select the width wil be restored
+ * @author	Tijs Verkoyen <tijs@sumocoders.be>
  */
-$('.ie7 select').mouseenter(function() { $(this).data('original-width', $(this).width()).css('width', 'auto'); });
-$('.ie7 select').mouseout(function () { $(this).css('width', $(this).data('original-width') +'px'); });
+// fix fault behaviour for selectboxes in IE
+function fixSelectWidthForIE() {
+	// only target IE
+	if($.browser.msie)
+	{
+		// bind mousedown
+		$('select:not([multiple])').mousedown(function()
+		{
+			// not already auto-width
+			if($(this).css('width') != 'auto')
+			{
+				// get current width
+				var width = $(this).width();
+
+				// reset if needed
+				if(typeof $(this).data('orginal-width') != 'undefined' && width > $(this).data('orginal-width')) width = $(this).data('orginal-width');
+
+				// set data and new width
+				$(this).data('orginal-width', width);
+				$(this).css('width', 'auto');
+
+				// if width is less then before, undo this
+				if($(this).width() < width)
+				{
+					// unbind
+					$(this).unbind('mousedown');
+
+					// reset width
+					$(this).css('width', $(this).data('orginal-width').toString() +'px');
+				}
+			}
+		})
+
+		// bind blur, if the user doesn't change the value
+		.blur(function() {
+			$(this).css('width', $(this).data('orginal-width').toString() +'px');
+		})
+
+		// bind change, if the user changes the value
+		.change(function() {
+			$(this).css('width', $(this).data('orginal-width').toString() +'px');
+		});
+    }
+}
+
+// call the function
+fixSelectWidthForIE();
