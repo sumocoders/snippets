fb19165a6887338dc8a6ba1206fe7ca020623a25
Tijs Verkoyen <tijs@verkoyen.eu>
2012-01-16 14:52:01 +0100

    Added demo + fixed to be compatible with the new api

--- javascript/facebook_share/share.js
+++ javascript/facebook_share/share.js
@@ -29,37 +29,28 @@ $('.shareFacebook').live('click', function(evt) {
 	// log in
 	FB.login(function(response) {
 		// any response
-		if(response.session) {
-			// required permissions
-			if(response.perms) 
-			{
-				// post to feed
-				FB.api('/me/feed', 'post', 
-					{ 
-						message: element.data('description'),
-						picture: element.data('image'),
-						link: element.data('url'),
-						name: element.data('title'),
-						description: element.data('description')
-					}, 
-					function(response) {
-						// not published
-						if(!response || response.error) $('#facebookError').html('<strong>Facebook:</strong> er ging iets mis, probeer het later opnieuw.').slideDown();
-						
-						// published
-						else
-						{
-							$('#facebookSuccess').html('<strong>Facebook:</strong> de link is gepubliceerd op je wall.').slideDown();
-							setTimeout(function() { $('#facebookSuccess').slideUp(); }, 3000);
-						}
-					});
-			} 
-			
-			// user is logged in, but did not grant any permissions
-			else 
-			{
-				$('#facebookError').html('<strong>Facebook:</strong> om deze link te delen moet je onze applicatie voldoende rechten geven.').slideDown();
-			}
+		if(response.authResponse) {
+			// post to feed
+			FB.api('/me/feed', 'post', 
+				{ 
+					message: element.data('description'),
+					picture: element.data('image'),
+					link: element.data('url'),
+					name: element.data('title'),
+					description: element.data('description')
+				}, 
+				function(response) {
+					// not published
+					if(!response || response.error) $('#facebookError').html('<strong>Facebook:</strong> er ging iets mis, probeer het later opnieuw.').slideDown();
+					
+					// published
+					else
+					{
+						$('#facebookSuccess').html('<strong>Facebook:</strong> de link is gepubliceerd op je wall.').slideDown();
+						setTimeout(function() { $('#facebookSuccess').slideUp(); }, 3000);
+					}
+				}
+			);
 		} 
 		
 		// user is not logged in.
@@ -67,5 +58,5 @@ $('.shareFacebook').live('click', function(evt) {
 		{
 			$('#facebookError').html('<strong>Facebook:</strong> om deze link te delen moet je onze applicatie voldoende rechten geven.').slideDown();
 		}
-	}, { perms: 'publish_stream' });
+	}, { scope: 'publish_stream' });
 });
