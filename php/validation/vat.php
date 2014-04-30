e91ed9cd17b95cc86267a64e1aa4e3c25b5312d4
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2014-04-30 15:49:34 +0200

    European VAT number validation

--- php/validation/vat.php
+++ php/validation/vat.php
@@ -5,6 +5,7 @@
  */
 function sanitizeVatNumber($number) {
     $sanitized = str_replace(array(' ', '/', '.', '-'), '', $number);
+    $number = strtoupper($number);
     return $sanitized;
 }
 
@@ -12,10 +13,17 @@ function sanitizeVatNumber($number) {
  * Check if the input is a VAT number (sanitize first)
  */
 function isVatNumber($number) {
-    return (
-        SpoonFilter::isAlphabetical(substr($number, 0, 2))
-        && SpoonFilter::isNumeric(substr($number, 2))
-        && strlen($number) >= 11
-        && strlen($number) <= 12
+    $rules = array(
+        'AT' => '/^ATU\d{8}$/', 'BE' => '/^BE[0-1]?\d{9}$/', 'BG' => '/^BG\d{9,10}$/', 'CZ' => '/^CZ\d{8,10}$/',
+        'CY' => '/^CY\d{8}[A-Z]$/', 'DE' => '/^DE\d{9}$/', 'DK' => '/^DK\d{8}$/', 'EE' => '/^EE\d{9}$/',
+        'EL' => '/^EL\d{9}$/', 'ES' => '/^ES([A-Z]\d{8}|\d{8}[A-Z])$/', 'FI' => '/^FI\d{8}$/',
+        'FR' => '/^FR\w{2}\d{9}$/', 'GB' => '/^GB(\d{9}|\d{12}|GD\d{3}|HA\d{3})$/', 'HR' => '/HR\d{11}$/', 
+        'HU' => '/^HU\d{8}$/', 'IE' => '/^IE(\d[A-Z0-9+*]\d{5}[A-Z]|\d{7}[A-Z]{1,2})$/', 'IT' => '/^IT\d{11}$/',
+        'LT' => '/^LT(\d{9}|\d{12})$/', 'LU' => '/^LU\d{8}$/', 'LV' => '/^LV\d{11}$/', 'MT' => '/^MT\d{8}$/', 
+        'NL' => '/^NL\d{9}B\d{2}$/', 'PL' => '/^PL\d{10}$/', 'PT' => '/^PT\d{9}$/', 'RO' => '/^RO\d{2,10}$/',
+        'SE' => '/^SE\d{12}$/', 'SI' => '/^SI\d{8}$/', 'SK' => '/^SK\d{10}$/'
     );
+    $countryCode = substr($number, 0, 2);
+    return preg_match($rules[$countryCode], $number);
 }
+
