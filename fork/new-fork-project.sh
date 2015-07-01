9a40e75b16a6efdc12f8e9cb5598521f4b0c8c86
Daan De Deckere <daan@sumocoders.be>
2015-07-01 13:33:07 +0200

    Use more descriptive variables and check # of arguments

--- fork/new-fork-project.sh
+++ fork/new-fork-project.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-mkdir -p ~/Sites/$2/$3
-cd ~/Sites/$2/$3
+if [[ $# -ne 3 ]] ; then
+  echo 'Usage: ./new-fork-project.sh <git-repo> <client> <project>'
+  exit 1
+fi
+
+repository=$1
+client=$2
+project=$3
+
+mkdir -p ~/Sites/$client/$project
+cd ~/Sites/$client/$project
 git clone git://github.com/sumocoders/forkcms.git .
 rm -rf .git
 git init
 git add .
 git commit -m 'initial commit'
-git remote add origin $1
+git remote add origin $repository
 git push --set-upstream origin master
 git checkout -b staging
 git push --set-upstream origin staging
 echo -e "You will now be taken to Gitlab. Please set the default branch to 'staging'. Press enter when you're done."
 read input_variable
-open http://git.sumocoders.be/sumocoders/$3/edit
+open http://git.sumocoders.be/sumocoders/$project/edit
 
-sed -i -e 's/set :client,  ""/set :client, "'$2'"/g' Capfile
-sed -i -e 's/set :project, ""/set :project, "'$3'"/g' Capfile
-sed -i -e 's|set :repository, ""|set :repository, "'$1'"|g' Capfile
+sed -i -e 's/set :client,  ""/set :client, "'$client'"/g' Capfile
+sed -i -e 's/set :project, ""/set :project, "'$project'"/g' Capfile
+sed -i -e 's|set :repository, ""|set :repository, "'$repository'"|g' Capfile
 
-sed -i -e 's|site.path_www:          </home/sites/<client>/<project>path>|site.path_www:          /home/sites/'$2'/'$3'|g' app/config/parameters_install.yml
+sed -i -e 's|site.path_www:          </home/sites/<client>/<project>path>|site.path_www:          /home/sites/'$client'/'$project'|g' app/config/parameters_install.yml
 
 git add .
 git commit -m 'Added basic project info to be able to deploy'
@@ -34,7 +43,7 @@ npm install
 grunt build
 
 echo -e "The Fork installer will now be opened. The database settings will be printed below."
-open http://$3.$2.dev/
+open http://$project.$client.dev/
 database=$(cap sumodev:db:info 2>&1 | grep database -A 2 | tail -n 1 | sed -e 's/ \*\* \[out :: dev.sumocoders.be\] //g')
 user=$(cap sumodev:db:info 2>&1 | grep user -A 3 | tail -n 1 | sed -e 's/ \*\* \[out :: dev.sumocoders.be\] //g')
 pass=$(cap sumodev:db:info 2>&1 | grep pass -A 3 | tail -n 1 | sed -e 's/ \*\* \[out :: dev.sumocoders.be\] //g')
@@ -44,7 +53,7 @@ echo "Password: $pass"
 echo -e "Please press enter when the installation has successfully completed. If you have problems with the installer, enter 'sumo box ssh' and do composer install in the project folder"
 read input_variable
 
-open http://$3.$2.dev/private
+open http://$project.$client.dev/private
 echo -e "Now import the locale.xml from the theme folder to the translations. Press enter when you're done"
 read input_variable
 
@@ -52,13 +61,13 @@ cp app/config/parameters.yml app/config/parameters.dev.yml
 sed -i -e 's/database.host:          10.11.12.13/database.host:          localhost/g' app/config/parameters.dev.yml
 sed -i -e 's/database.user:          root/database.user:          '$user'/g' app/config/parameters.dev.yml
 sed -i -e 's/database.password:      root/database.password:      '$pass'/g' app/config/parameters.dev.yml
-sed -i -e 's/site.domain:            '$3'.'$2'.dev/site.domain:            '$3'.'$2'.sumocoders.eu/g' app/config/parameters.dev.yml
+sed -i -e 's/site.domain:            '$project'.'$client'.dev/site.domain:            '$project'.'$client'.sumocoders.eu/g' app/config/parameters.dev.yml
 
 cap deploy:setup deploy
 cap sumodev:db:put
-scp app/config/parameters.dev.yml sites@dev.sumocoders.eu:~/apps/$2/$3/shared/config/parameters.yml
+scp app/config/parameters.dev.yml sites@dev.sumocoders.eu:~/apps/$client/$project/shared/config/parameters.yml
 
-open http://$3.$2.sumocoders.eu/
+open http://$project.$client.sumocoders.eu/
 echo -e "Please check whether the site is working correctly on staging."
 read input_variable
 
