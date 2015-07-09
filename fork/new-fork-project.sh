577dbb57c01ad887b712f35fb369484c259a91d9
Wouter Sioen <wouter.sioen@gmail.com>
2015-07-09 11:14:06 +0200

    Fix installation of locale

--- fork/new-fork-project.sh
+++ fork/new-fork-project.sh
@@ -59,7 +59,7 @@ echo "Password: $pass"
 echo -e "Please press return when the installation has successfully completed. If you have problems with the installer, enter 'sumo box ssh' and do composer install in the project folder"
 read input_variable
 
-php tools/import_locale.php -f src/Frontend/Themes/Bootstrap/locale.xml
+php tools/install_locale.php -f src/Frontend/Themes/Bootstrap/locale.xml
 
 cp app/config/parameters.yml app/config/parameters.dev.yml
 sed -i '' -e 's/database.host:.*/database.host:          localhost/g' app/config/parameters.dev.yml
